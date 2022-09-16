340a2a9d5169f88eef7cee6d9966d2193e6dc5b9
Shinya.Umeda <umeda@barnet.co.jp>
2022-09-16 16:55:50 +0900

    TcpCtrl追加

--- MauiCtrl/MauiCtrl/PageCtrl.cs
+++ MauiCtrl/MauiCtrl/PageCtrl.cs
@@ -1,9 +1,28 @@
 ﻿using Microsoft.Maui.Controls;
+using System;
 
 namespace MauiCtrl
 {
     public class PageCtrl
     {
+	    //**********************************************************************************
+        //NewPage pg = new NewPage();
+        public bool openPageNavi(NavigableElement navigable_element, Page pg, bool bl_animation)
+        {
+            try
+            {
+                navigable_element.Navigation.PushAsync(pg, bl_animation);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
 
+        public void test(Func<Page, bool, Task> push_async, Page pg, bool bl_animation)
+        {
+            push_async(pg, bl_animation);
+        }
     }
 }
---
+++ MauiCtrl/MauiCtrl/TcpCtrl.cs
@@ -0,0 +1,64 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+/*
+ * 参考　https://t19488sns.com/socket/370/
+ * 
+ */
+
+namespace MauiCtrl
+{
+
+    public class TcpCtrl
+    {
+	//**********************************************************************************
+        public TcpClient connectTcp(string ipAdd, int port)
+        {
+            return new TcpClient(ipAdd, port);
+        }
+
+	//**********************************************************************************
+        public NetworkStream getNetworkStream(TcpClient tcp)
+        {
+            return tcp.GetStream();
+        }
+
+        //**********************************************************************************
+        public void Send_TCP(NetworkStream ns, string send_mess)
+        {
+            ns.ReadTimeout = 1000;
+            ns.WriteTimeout = 1000;
+            Encoding enc = Encoding.UTF8;
+            byte[] sendBytes = enc.GetBytes(send_mess);
+            ns.Write(sendBytes, 0, sendBytes.Length);
+        }
+
+	//**********************************************************************************
+        public string Recieve_TCP(NetworkStream ns)
+        {
+            ns.ReadTimeout = 10000;
+            ns.WriteTimeout = 10000;
+            Encoding enc = Encoding.UTF8;
+            string rcv = "";
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                byte[] resBytes = new byte[1024];
+                int resSize = 0;
+
+                resSize = ns.Read(resBytes, 0, resBytes.Length);
+
+                if (resSize == 0)
+                    Console.WriteLine("サーバーが切断しました。");
+                else
+                {
+                    ms.Write(resBytes, 0, resSize);
+
+                    rcv = enc.GetString(ms.GetBuffer(), 0, (int)ms.Length);
+                    ms.Close();
+                }
+            }
+            return rcv;
+        }
+    }
+}
