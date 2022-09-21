84315999a039e85383bf637dc74ac740563ad4d3
Shinya.Umeda <umeda@barnet.co.jp>
2022-09-21 18:20:06 +0900

    no message

--- MauiCtrl/MauiCtrl/MessageCtrl.cs
+++ MauiCtrl/MauiCtrl/MessageCtrl.cs
@@ -1,11 +1,30 @@
 ﻿
+using static System.Net.Mime.MediaTypeNames;
+using System.Threading.Tasks;
+using System.Threading;
+
 namespace MauiCtrl
 {
     public class MessageCtrl
     {
+        CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
         public async void MessageAlert(string str_title, string str_message, string str_btn)
         {
-            
         }
+
+        //public async void MessageToast()
+        //{
+        //    using CommunityToolkit.Maui.Alerts;
+
+        //    CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+        //    string text = "This is a Toast";
+        //    ToastDuration duration = ToastDuration.Short;
+        //    double fontSize = 14;
+
+        //    var toast = Toast.Make(text, duration, fontSize);
+
+        //    await toast.Show(cancellationTokenSource.Token);
+        //}
     }
 }
---
+++ MauiCtrl/MauiCtrl/TextCtrl.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace MauiCtrl
+{
+    public class TextCtrl
+    {
+        //**********************************************************************************
+        /// <summary>
+        /// テキストから数字を抽出
+        /// </summary>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public int extractNum(string val)
+        {
+            return int.Parse(Regex.Replace(val, @"[^0-9]", ""));
+        }
+    }
+}
